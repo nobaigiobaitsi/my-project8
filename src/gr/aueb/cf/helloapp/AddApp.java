50fc66273cc1306e2335d4e4cdacd51f131d1f6b
Stavros <steveangelopoulos@gmail.com>
2025-05-15 13:38:33 +0300

    fix: add correct addition in main branch

--- src/gr/aueb/cf/helloapp/AddApp.java
+++ src/gr/aueb/cf/helloapp/AddApp.java
@@ -3,6 +3,7 @@ package gr.aueb.cf.helloapp;
 public class AddApp {
     public static void main(String[] args) {
         System.out.println(add(3, 5));
+        System.out.printf("%4d", add(7, 2));
     }
     public static int add(int a, int b){
         return a + b;
